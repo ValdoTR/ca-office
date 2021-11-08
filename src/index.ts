6181dbae001decc521c078acfdac2c21c33ffcc8
ValdoTR <vteixeiraromao@gmail.com>
2021-11-08 18:01:49 +0000

    Deploying to gh-pages from master @ e8266cce4f8bbd7de8013530e0c2065610a7d92b 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,59 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
-
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
-
+let currentZone: string;
 let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
+const config = [
+    {
+        zone: 'needHelp',
+        message: 'Avez-vous besoin d\'aide ? Nous serons ravis de vous aider.',
+        cta: [
+            {
+                label: 'Rejoindre',
+                className: 'primary',
+                callback: () => WA.nav.openTab('https://play.staging.workadventu.re/@/tcm/workadventure/wa-village'),
+            }
+        ]
+    },
+    {
+        zone: 'followUs',
+        message: 'Suivez-nous sur les réseaux sociaux !',
+        cta: [
+            {
+                label: 'LinkedIn',
+                className: 'primary',
+                callback: () => WA.nav.openTab('https://www.linkedin.com/company/workadventu-re'),
+            },
+            {
+                label: 'Twitter',
+                className: 'primary',
+                callback: () => WA.nav.openTab('https://twitter.com/workadventure_'),
+            }
+        ]
+    },
+]
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onEnterZone('needHelp', () => {
+    currentZone = 'needHelp'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.room.onEnterZone('followUs', () => {
+    currentZone = 'followUs'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.room.onLeaveZone('needHelp', closePopup);
+WA.room.onLeaveZone('followUs', closePopup);
 
-function closePopUp(){
+function openPopup(zoneName: string, popupName: string) {
+    const zone = config.find((item) => {
+        return item.zone == zoneName
+    });
+    if (typeof zone !== 'undefined') {
+        // @ts-ignore otherwise we can't use zone.cta object
+        currentPopup = WA.ui.openPopup(popupName, zone.message, zone.cta)
+    }
+}
+function closePopup(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
