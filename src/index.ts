f035b9c15112f09352106a1f85899742e70d509b
ValdoTR <vteixeiraromao@gmail.com>
2022-01-10 15:11:36 +0000

    Deploying to gh-pages from master @ 54efd722f3db5d7dcbab6b2e6f786dc50ab9d002 🚀

--- src/index.ts
+++ src/index.ts
@@ -16,20 +16,19 @@ const config = [
         ]
     },
     {
-        zone: 'followUs',
-        message: 'Suivez-nous sur les réseaux sociaux !',
-        cta: [
-            {
-                label: 'LinkedIn',
-                className: 'primary',
-                callback: () => WA.nav.openTab('https://www.linkedin.com/company/workadventu-re'),
-            },
-            {
-                label: 'Twitter',
-                className: 'primary',
-                callback: () => WA.nav.openTab('https://twitter.com/workadventure_'),
-            }
-        ]
+        zone: 'joystickPirate',
+        message: 'Pour jouer entre vous, entrez le même nom de "room" puis lancez la partie !',
+        cta: []
+    },
+    {
+        zone: 'joystickUno',
+        message: 'Pour jouer entre vous, entrez le même nom de "room" puis lancez la partie !',
+        cta: []
+    },
+    {
+        zone: 'joystickPong',
+        message: 'Pour jouer entre vous, entrez le même nom de "room" puis lancez la partie !',
+        cta: []
     },
 ]
 
@@ -41,8 +40,24 @@ WA.room.onEnterZone('followUs', () => {
     currentZone = 'followUs'
     openPopup(currentZone, currentZone + 'Popup')
 });
+WA.room.onEnterLayer('joystickPirate').subscribe(() => {
+    currentZone = 'joystickPirate'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.room.onEnterLayer('joystickUno').subscribe(() => {
+    currentZone = 'joystickUno'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.room.onEnterLayer('joystickPong').subscribe(() => {
+    currentZone = 'joystickPong'
+    openPopup(currentZone, currentZone + 'Popup')
+});
 WA.room.onLeaveZone('needHelp', closePopup);
 WA.room.onLeaveZone('followUs', closePopup);
+WA.room.onLeaveLayer('joystickPirate').subscribe(closePopup);
+WA.room.onLeaveLayer('joystickUno').subscribe(closePopup);
+WA.room.onLeaveLayer('joystickPong').subscribe(closePopup);
+
 
 function openPopup(zoneName: string, popupName: string) {
     const zone = config.find((item) => {
